4668285e8a21a0680679f8657120845c4c12a2b1
ducc <spielerc@gmail.com>
2023-12-27 14:07:13 -0700

    Add HeadBob

---
+++ code/HeadBob.cs
@@ -0,0 +1,31 @@
+using System;
+using Sandbox;
+
+public sealed class HeadBob : Component
+{
+	[Property] public CharacterController Controller { get; set; }
+	[Property] public GameObject Target { get; set; }
+	[Property] public float BobSpeed { get; set; } = 10f;
+	[Property] public float BobAmplitude { get; set; } = 0.1f;
+	[Property] public float CurrentYBob { get; private set; }
+	[Property] public float CurrentZBob { get; private set; }
+	[Property] public Vector3 InitialPosition { get; private set; }
+
+	protected override void OnStart()
+	{
+		InitialPosition = Target.Transform.LocalPosition;
+	}
+
+	protected override void OnUpdate()
+	{
+		if ( !Controller.IsOnGround )
+			return;
+
+		var velocityFactor = Controller.Velocity.Length.LerpInverse( 0f, 200f );
+		CurrentYBob = MathF.Sin( Time.Now * BobSpeed / 2f ) * BobAmplitude;
+		CurrentZBob = MathF.Cos( Time.Now * BobSpeed ) * BobAmplitude;
+		var bobVector = new Vector3( 0f, CurrentYBob, CurrentZBob );
+		var scaledBob = Vector3.Zero.LerpTo( bobVector, velocityFactor );
+		Target.Transform.LocalPosition = InitialPosition + scaledBob;
+	}
+}
