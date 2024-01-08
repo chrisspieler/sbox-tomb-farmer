7f989432c5982eb24c72ed63582b82e9d027b4f3
ducc <spielerc@gmail.com>
2024-01-08 10:22:40 -0700

    Add pillars to the outside corners of walls

--- code/MapGenerator.cs
+++ code/MapGenerator.cs
@@ -14,44 +14,79 @@ public sealed class MapGenerator : Component
 	[Property] public Model CellCorner { get; set; }
 	[Property] public Model Cell3Way { get; set; }
 	[Property] public Model Cell4Way { get; set; }
+	[Property] public Model Pillar { get; set; }
 	[Property] public float VisualizeDelay { get; set; } = 0.5f;
 	[Property] public GameObject PlayerPrefab { get; set; }
+	[Property] public bool Visualize { get; set; } = false;
+
+	public const float CELL_SIZE = 128f;
 
 	private Dictionary<Vector3Int, GameObject> _cells = new();
 
 	protected override void OnStart()
 	{
-		VisualizeGeneration();
+		RunGeneration();
 	}
 
-	public async void VisualizeGeneration()
+	public async void RunGeneration()
 	{
 		var light = Scene.GetAllComponents<DirectionalLight>().FirstOrDefault();
 		var camera = Components.Get<CameraComponent>();
 		foreach(var cell in GenerateCells() )
 		{
-			if ( camera.IsValid() )
+			if ( Visualize )
 			{
-				camera.Transform.Rotation = Rotation.LookAt( cell.Transform.Position - Transform.Position );
+				await VisualizeObject( cell, camera );
 			}
-			_ = FlashTint( cell, VisualizeDelay * 4f, Color.Blue );
-			await Task.DelaySeconds( VisualizeDelay );
 		}
 		foreach( var cell in AutotileCells() )
 		{
-			if ( camera.IsValid() )
+			if ( Visualize )
 			{
-				camera.Transform.Rotation = Rotation.LookAt( cell.Transform.Position - Transform.Position );
+				await VisualizeObject( cell, camera );
 			}
-			_ = FlashTint( cell, VisualizeDelay * 4f, Color.Cyan );
-			await Task.DelaySeconds( VisualizeDelay );
+		}
+		foreach( var pillar in CreatePillars() )
+		{
+			if ( Visualize )
+			{
+				await VisualizeObject( pillar, camera );
+			}
 		}
 
 		if ( light.IsValid() ) light.Enabled = false;
 		if ( camera.IsValid() ) camera.Enabled = false;
 
-		var spawnPos = _cells.Skip(1).First().Value.Transform.Position + new Vector3( 0, 0, 0 );
+		var spawnPos = _cells.First().Value.Transform.Position + new Vector3( 0, 0, 0 );
 		var player = SceneUtility.Instantiate( PlayerPrefab, spawnPos );
+	}
+
+	private async Task VisualizeObject( GameObject go, CameraComponent camera )
+	{
+		if ( camera.IsValid() )
+		{
+			camera.Transform.Rotation = Rotation.LookAt( go.Transform.Position - camera.Transform.Position );
+		}
+		_ = FlashTint( go, VisualizeDelay * 4f, Color.Cyan );
+		await Task.DelaySeconds( VisualizeDelay );
+	}
+
+	private async Task FlashTint( GameObject go, float duration, Color tintColor )
+	{
+		var renderer = go.Components.Get<ModelRenderer>( FindMode.EverythingInSelfAndDescendants );
+		if ( !renderer.IsValid() ) return;
+
+
+		renderer.Tint = tintColor;
+		TimeUntil undoTint = duration;
+		while ( !undoTint )
+		{
+			renderer.Tint = Color.Lerp( tintColor, Color.White, undoTint.Fraction );
+			await Task.Frame();
+		}
+
+		if ( !renderer.IsValid() ) return;
+		renderer.Tint = Color.White;
 	}
 
 	public IEnumerable<GameObject> GenerateCells()
@@ -109,10 +144,10 @@ public sealed class MapGenerator : Component
 	private GameObject GenerateCell( Vector3Int position )
 	{
 		var cell = new GameObject( true, $"Cell {position}" );
-		cell.Transform.Position = new Vector3( position.x * 128, position.y * 128, 0 );
+		cell.Transform.Position = new Vector3( position.x * CELL_SIZE, position.y * CELL_SIZE, 0 );
 		var model = new GameObject( true, $"Model {position}" );
 		model.Parent = cell;
-		model.Transform.LocalPosition = new Vector3( -64, 64 );
+		model.Transform.LocalPosition = new Vector3( -(CELL_SIZE / 2), CELL_SIZE / 2 );
 		var renderer = model.Components.Create<ModelRenderer>();
 		renderer.Model = CellModel;
 		var collider = model.Components.Create<ModelCollider>();
@@ -139,7 +174,6 @@ public sealed class MapGenerator : Component
 			if ( BitOperations.PopCount( (byte)neighbors ) == 1 )
 			{
 				SetModel( kvp.Value, Cell1Way );
-				// TODO: Find out why this sometimes isn't lining up.
 				kvp.Value.Transform.Rotation = neighbors switch
 				{
 					Neighbors.Forward => Rotation.FromYaw( 0 ),
@@ -208,33 +242,120 @@ public sealed class MapGenerator : Component
 			var collider = cell.Components.Get<ModelCollider>( FindMode.EverythingInSelfAndDescendants );
 			collider.Model = model;
 		}
-
-		Neighbors GetNeighbors( Vector3Int cell )
-		{
-			Neighbors neighbors = 0;
-			if ( _cells.ContainsKey( cell + new Vector3Int( 1, 0 ) ) ) neighbors |= Neighbors.Forward;
-			if ( _cells.ContainsKey( cell + new Vector3Int( 0, -1 ) ) ) neighbors |= Neighbors.Right;
-			if ( _cells.ContainsKey( cell + new Vector3Int( -1, 0 ) ) ) neighbors |= Neighbors.Backward;
-			if ( _cells.ContainsKey( cell + new Vector3Int( 0, 1 ) ) ) neighbors |= Neighbors.Left;
-			return neighbors;
-		}
 	}
 
-	private async Task FlashTint( GameObject go, float duration, Color tintColor ) 
+	private Neighbors GetNeighbors( Vector3Int cell )
 	{
-		var renderer = go.Components.Get<ModelRenderer>( FindMode.EverythingInSelfAndDescendants );
-		if ( !renderer.IsValid() ) return;
+		Neighbors neighbors = 0;
+		if ( _cells.ContainsKey( cell + new Vector3Int( 1, 0 ) ) ) neighbors |= Neighbors.Forward;
+		if ( _cells.ContainsKey( cell + new Vector3Int( 0, -1 ) ) ) neighbors |= Neighbors.Right;
+		if ( _cells.ContainsKey( cell + new Vector3Int( -1, 0 ) ) ) neighbors |= Neighbors.Backward;
+		if ( _cells.ContainsKey( cell + new Vector3Int( 0, 1 ) ) ) neighbors |= Neighbors.Left;
+		return neighbors;
+	}
 
+	private Vector3Int GetCornerCoords( Vector3Int cell, Corner corner )
+	{
+		return cell + GetCornerOffset( corner );
+	}
 
-		renderer.Tint = tintColor;
-		TimeUntil undoTint = duration;
-		while ( !undoTint )
+	private Vector3Int GetCornerOffset( Corner corner )
+	{
+		// TODO: Consider making this an extension method on Corner.
+		return corner switch
 		{
-			renderer.Tint = Color.Lerp( tintColor, Color.White, undoTint.Fraction );
-			await Task.Frame();
+			Corner.ForwardRight => new Vector3Int( 1, -1 ),
+			Corner.RightBackward => new Vector3Int( -1, -1 ),
+			Corner.BackwardLeft => new Vector3Int( -1, 1 ),
+			Corner.LeftForward => new Vector3Int( 1, 1 ),
+			_ => Vector3Int.Zero
+		};
+	}
+
+	private bool HasCorner( Vector3Int cell, Corner corner )
+	{
+		var offset = corner switch
+		{
+			Corner.ForwardRight => new Vector3Int( 1, -1 ),
+			Corner.RightBackward => new Vector3Int( -1, -1 ),
+			Corner.BackwardLeft => new Vector3Int( -1, 1 ),
+			Corner.LeftForward => new Vector3Int( 1, 1 ),
+			_ => Vector3Int.Zero
+		};
+		return _cells.ContainsKey( cell + offset );
+	}
+
+	private enum Corner
+	{
+		ForwardRight,
+		RightBackward,
+		BackwardLeft,
+		LeftForward
+	}
+
+	private Vector3 GetCornerPosition( GameObject go, Corner corner )
+	{
+		var offset = (Vector3)GetCornerOffset( corner ) * (CELL_SIZE / 2);
+		var position = go.Transform.Position + offset;
+		// For now, assume that floor level is 8 units above the origin.
+		position += Vector3.Up * 8f;
+		return position;
+	}
+
+	private List<Corner> GetCorners( Vector3Int cell )
+	{
+		var corners = new List<Corner>();
+		foreach( var corner in Enum.GetValues<Corner>() )
+		{
+			if ( HasCorner( cell, corner ) )
+			{
+				corners.Add( corner );
+			}
+		}
+		return corners;
+	}
+
+	/// <summary>
+	/// Creates pillars on the outside corners of walls to hide seams.
+	/// </summary>
+	private IEnumerable<GameObject> CreatePillars()
+	{
+		HashSet<Vector3Int> _processedCells = new();
+		foreach ( var cell in _cells )
+		{
+			foreach( var corner in GetCorners( cell.Key ) )
+			{
+				var coords = GetCornerCoords( cell.Key, corner );
+				// Don't add a pillar twice for the same corner.
+				if ( _processedCells.Contains( coords ) )
+				{
+					continue;
+				}
+				var offset = GetCornerOffset( corner );
+				var xNeighbor = cell.Key + new Vector3Int( offset.x, 0 );
+				var yNeighbor = cell.Key + new Vector3Int( 0, offset.y );
+				// If this cell, the diagonal cell, and the two neighbor cells all exist, then
+				// this pillar is inside of a room. It won't be added.
+				if ( _cells.ContainsKey( xNeighbor ) && _cells.ContainsKey( yNeighbor ) )
+				{
+					continue;
+				}
+				var position = GetCornerPosition( cell.Value, corner );
+				yield return CreatePillar( position, cell.Value );
+			}
+			_processedCells.Add( cell.Key );
 		}
 
-		if ( !renderer.IsValid() ) return;
-		renderer.Tint = Color.White;
+		GameObject CreatePillar( Vector3 position, GameObject parent )
+		{
+			var go = new GameObject( true, "Pillar" );
+			go.Parent = parent;
+			go.Transform.Position = position;
+			var renderer = go.Components.Create<ModelRenderer>();
+			renderer.Model = Pillar;
+			var collider = go.Components.Create<ModelCollider>();
+			collider.Model = Pillar;
+			return go;
+		}
 	}
 }
